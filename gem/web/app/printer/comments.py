26d4cf53a11e8d14c1338c19f6bda3062b77af9e
Advaita Krishna das <advaita.krishna.das@gmail.com>
2018-01-12 16:25:42 +0300

    #112 rename

--- gem/web/app/printer/comments.py
+++ gem/web/app/printer/comments.py
@@ -2,7 +2,7 @@ from jinja2 import Template
 import pdfkit
 
 
-def print_comments(data):
+def print_comments(filter):
     template = Template('Hello {{ name }}!')
     html = template.render(name='John Doe')
     pdfkit.from_string(html, "/Users/akd/Desktop/1.pdf", options={"zoom": 8})
