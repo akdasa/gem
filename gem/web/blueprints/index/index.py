4fae879f9d6b557504a4eb59ded03f2c8ea27338
Advaita Krishna das <advaita.krishna.das@gmail.com>
2018-01-15 16:17:50 +0300

    Display active session #82

--- gem/web/blueprints/index/index.py
+++ gem/web/blueprints/index/index.py
@@ -9,12 +9,19 @@ index = Blueprint("index", __name__, template_folder=".")
 @index.route("/", methods=["GET", "POST"])
 def index_index():
     return render_template("index_index.html",
-                           sessions=__upcoming_sessions_for_user(current_user))
+                           upcoming_sessions=__upcoming_sessions_for_user(current_user),
+                           active_sessions=__active_sessions_for_user(current_user))
 
 
 def __upcoming_sessions_for_user(user):
     if user and hasattr(user, "role"):
         upcoming = sessions.upcoming()
-        result = filter(lambda x: user.role in x["permissions"]["presence"], upcoming)
-        return list(result)[:3]
+        result = filter(lambda x: x.get("status", None) != "run", upcoming)
+        return list(result)[:5]
+    return []
+
+def __active_sessions_for_user(user):
+    if user and hasattr(user, "role"):
+        upcoming = sessions.active()
+        return list(upcoming)[:3]
     return []
