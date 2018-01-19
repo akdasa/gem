d3562d10c3e580b669365376d97fe9e3e1a1ae36
Advaita Krishna das <advaita.krishna.das@gmail.com>
2018-01-19 15:38:44 +0300

    #112 configs

--- gem/web/app/printer/comments.py
+++ gem/web/app/printer/comments.py
@@ -1,18 +1,22 @@
 import pdfkit
 import dateutil.parser
+import configparser
 from jinja2 import Template
 
 from gem.db import comments, users
 
 
 def print_comments(requester, criteria):
+    conf = __config()
+    path_to_printed = conf.get("[printer]", "path", fallback="temp/printed")
+
     p = "./gem/web/app/printer/"
     template_file = open(p + "comments_template.html", "r").read()
     template = Template(template_file)
     rows = map(__map, comments.find(criteria))
     html = template.render(requester=requester, rows=rows)
-    pdfkit.from_string(html, "/Users/akd/Desktop/1.pdf", options={"zoom": 6})
-    return "/Users/akd/Desktop/1.pdf"
+    pdfkit.from_string(html, path_to_printed + "/1.pdf", options={"zoom": 6})
+    return path_to_printed + "/1.pdf"
 
 
 def __map(r):
@@ -20,3 +24,8 @@ def __map(r):
     if hasattr(r, "timestamp"):
         r.timestamp = dateutil.parser.parse(r.timestamp).strftime("%H:%M:%S")
     return r
+
+def __config():
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    return config
